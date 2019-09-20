a62f793a68694d4a8327cee9ba723974259d63e9
sv2 <sv2@slana.tech>
2019-09-20 15:59:49 -0700

    Initial

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./quasar";
+
+// Dashblocks CSS
+import "dashblocks/dist/dashblocks.css";
 
 Vue.config.productionTip = false;
 
---
+++ src/quasar.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+
+import "./styles/quasar.styl";
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/fontawesome-v5/fontawesome-v5.css";
+import "@quasar/extras/eva-icons/eva-icons.css";
+import Quasar from "quasar";
+
+Vue.use(Quasar, {
+  config: {}
+});
