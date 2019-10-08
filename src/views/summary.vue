700789646780ee799b0fecc7ae973c483ced696a
sv2 <sv2@slana.tech>
2019-10-07 21:59:18 -0700

    Initial

--- src/views/summary.vue
+++ src/views/summary.vue
@@ -25,13 +25,14 @@ export default {
       dbdata: new DbData(),
       dbspec: {
         layout: {
-          type: 'grid'
+          type: 'grid',
+          size: 12
         },
         widgets: [
           {
             id: 'w1',
             type: 'DbNumber',
-            cspan: 4,
+            cspan: 2,
             properties: {
               title: 'Requests',
               subtitle: 'Total requests received',
@@ -41,18 +42,18 @@ export default {
           {
             id: 'w2',
             type: 'DbNumber',
-            cspan: 3,
+            cspan: 2,
             properties: {
               title: 'Apdex Score',
               subtitle: 'Overall Apdex Score',
-              total: 0.01,
+              total: 1,
               format: '%.2f'
             }
           },
           {
             id: 'w3',
             type: 'DbNumber',
-            cspan: 3,
+            cspan: 2,
             properties: {
               title: 'Requests Rate',
               subtitle: 'Requests per second',
@@ -62,7 +63,7 @@ export default {
           {
             id: 'w4',
             type: 'DbNumber',
-            cspan: 3,
+            cspan: 2,
             properties: {
               title: 'Error Rate',
               subtitle: 'Errors per second',
@@ -72,17 +73,29 @@ export default {
           {
             id: 'w5',
             type: 'DbNumber',
-            cspan: 3,
+            cspan: 2,
             properties: {
               title: 'CPU',
               subtitle: 'CPU Usage',
-              format: '%.2f'
+              total: 100,
+              format: '%.2f %s',
+              qualifier: '%'
+            }
+          },
+          {
+            id: 'w6',
+            type: 'DbNumber',
+            cspan: 2,
+            properties: {
+              title: 'Memory',
+              subtitle: 'Memory Usage',
+              format: '%.2f %s'
             }
           },
           {
             id: 'w15',
             type: 'DbDygraphsBar',
-            cspan: 16,
+            cspan: 12,
             height: 250,
             properties: {
               options: {
@@ -124,9 +137,11 @@ export default {
     initialize: function() {
       // Init dashboard data
       this.dbdata.setWData('w1', { value: 0, trend: [] });
-      this.dbdata.setWData('w2', { value: 0 });
-      this.dbdata.setWData('w3', { value: 0 });
-      this.dbdata.setWData('w4', { value: 0 });
+      this.dbdata.setWData('w2', { value: 0, trend: [] });
+      this.dbdata.setWData('w3', { value: 0, trend: [] });
+      this.dbdata.setWData('w4', { value: 0, trend: [] });
+      this.dbdata.setWData('w5', { value: 0, trend: [] });
+      this.dbdata.setWData('w6', { value: 0, qualifier:'', trend: [] });
       this.dbdata.setWData('w15', { data: [] });
     },
 
@@ -137,27 +152,78 @@ export default {
       }, 1000);
     },
 
-
     updateStats: function() {
-
       // Update numbers
       this.dbdata.w1.value = pathOr(0, ['all', 'requests'], statsContainer);
       this.dbdata.w2.value = pathOr(0, ['all', 'apdex_score'], statsContainer);
       this.dbdata.w3.value = pathOr(0, ['all', 'req_rate'], statsContainer);
       this.dbdata.w4.value = pathOr(0, ['all', 'err_rate'], statsContainer);
+      this.dbdata.w5.value = pathOr(0, ['sys', 'cpu'], statsContainer);
+      // TODO Fix error - value should be number
+      //this.dbdata.setWData('w6', { value: , trend: [] });
+      let {value,qualifier} = this.formatBytes(pathOr(0, ['sys', 'heapUsed'], statsContainer), 2);
+      this.dbdata.w6.value = value;
+      this.dbdata.w6.qualifier = qualifier;
 
       this.dbdata.w1.trend = [];
+      this.dbdata.w2.trend = [];
+      this.dbdata.w3.trend = [];
+      this.dbdata.w4.trend = [];
+      this.dbdata.w5.trend = [];
+      this.dbdata.w6.trend = [];
 
       let timelineSorted = statsContainer.getSortedTimeline();
       let dthData = [];
+      //let trendReqData =  [];
       for (let entry of timelineSorted) {
         dthData.push([new Date(entry.ts), pathOr(0, ['stats', 'requests'], entry), pathOr(0, ['stats', 'errors'], entry)]);
         // Trend for requests
         this.dbdata.w1.trend.push(pathOr(0, ['stats', 'requests'], entry));
+        this.dbdata.w2.trend.push(pathOr(0, ['stats', 'apdex_score'], entry));
+        this.dbdata.w3.trend.push(pathOr(0, ['stats', 'req_rate'], entry));
+        this.dbdata.w4.trend.push(pathOr(0, ['stats', 'err_rate'], entry));
+        this.dbdata.w5.trend.push(pathOr(0, ['sys', 'cpu'], entry));
+        this.dbdata.w6.trend.push(pathOr(0, ['sys', 'heapUsed'], entry));
+        //trendReqData.push(pathOr(0, ['stats', 'requests'], entry));
       }
+      //trendReqData = this.collapse(trendReqData, 1);
+      //this.dbdata.w1.trend = trendReqData;
       this.dbdata.setWData('w15', { data: dthData });
       this.dbdata.touch('w1');
+      this.dbdata.touch('w2');
+      this.dbdata.touch('w3');
+      this.dbdata.touch('w4');
+      this.dbdata.touch('w5');
+      this.dbdata.touch('w6');
       this.loadStats();
+    },
+
+    formatBytes: function(a, b) {
+      if (0 === a) return {value:0,qualifier:'Bytes'};
+      let c = 1e3,
+        d = b || 2,
+        e = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+        f = Math.floor(Math.log(a) / Math.log(c));
+      return { value:parseFloat((a / Math.pow(c, f)).toFixed(d)),qualifier:e[f]};
+    },
+
+    collapse: function(array, chunkSize) {
+      let res = [];
+      let cidx = 0;
+      let aggr = 0;
+      for (let v of array) {
+        aggr += v;
+        cidx++;
+        if (cidx === chunkSize) {
+          res.push(aggr);
+          cidx = 0;
+          aggr = 0;
+        }
+      }
+      if (cidx > 0) {
+        res.push(aggr);
+      }
+      return res;
     }
   }
 };
