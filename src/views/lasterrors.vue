477fa30934cd81cb7f81fa88e6d64405421ee47e
sv2 <sv2@slana.tech>
2019-10-29 00:07:01 -0700

    Initial

--- src/views/lasterrors.vue
+++ src/views/lasterrors.vue
@@ -1,33 +1,43 @@
 <template>
   <q-page padding>
-    <div style="padding: 4px;">
-      <vue-good-table
-        :columns="columns"
-        :rows="rows"
-        styleClass="vgt-table condensed bordered striped sws-table"
-        :search-options="{
-          enabled: true,
-          skipDiacritics: true
-        }"
-        :pagination-options="{
-          enabled: true,
-          mode: 'records',
-          perPage: 30,
-          perPageDropdown: [10, 20, 30, 50, 80, 100],
-          dropdownAllowAll: true
-        }"
-      >
-        <template slot="table-row" slot-scope="props">
-          <span v-if="props.column.field == 'path'">
-            <!--<span style="font-weight: bold; color: blue;">{{ props.row.path }}</span>-->
-            <router-link :to="{ path: 'apiop', query: { method: props.row.method, path: props.row.path } }">{{ props.row.path }}</router-link>
-          </span>
-          <span v-else>
-            {{ props.formattedRow[props.column.field] }}
-          </span>
-        </template>
-      </vue-good-table>
-    </div>
+    <q-splitter v-model="splitterModel" :limits="[50, 100]">
+      <template v-slot:before>
+        <div style="padding: 4px;">
+          <vue-good-table
+            :columns="columns"
+            :rows="rows"
+            styleClass="vgt-table condensed bordered striped sws-table"
+            :search-options="{
+              enabled: true,
+              skipDiacritics: true
+            }"
+            :pagination-options="{
+              enabled: true,
+              mode: 'records',
+              perPage: 30,
+              perPageDropdown: [10, 20, 30, 50, 80, 100],
+              dropdownAllowAll: true
+            }"
+          >
+            <template slot="table-row" slot-scope="props">
+              <span v-if="props.column.field == 'api.path'">
+                <!--<span style="font-weight: bold; color: blue;">{{ props.row.path }}</span>-->
+                <router-link :to="{ path: 'apiop', query: { method: props.row.method, path: props.row.api.path } }">{{ props.row.api.path }}</router-link>
+              </span>
+              <span v-else>
+                {{ props.formattedRow[props.column.field] }}
+              </span>
+            </template>
+          </vue-good-table>
+        </div>
+      </template>
+
+      <template v-slot:after>
+        <div class="q-pa-md">
+          <pre><code ref="code" class="language-json"></code></pre>
+        </div>
+      </template>
+    </q-splitter>
   </q-page>
 </template>
 
@@ -36,37 +46,30 @@ import { pathOr } from 'ramda';
 import statsContainer from '@/store/statscontainer';
 import { mapState, mapActions } from 'vuex';
 import { vgtMethods } from '../mixins/vgtmethods';
+import Prism from 'prismjs';
+//import 'prismjs/themes/prism.css';
+import 'prismjs/themes/prism-tomorrow.css';
+import 'prismjs/components/prism-json';
 
 export default {
   name: 'ApiView',
-  components: {
-  },
+  components: {},
   mixins: [vgtMethods],
   data() {
     return {
+      splitterModel: 60, // start at 50%
       timer: null,
       isDark: false,
       columns: [
-        { label: 'Time', field: '@timestamp', tdClass: 'text-body2', type: 'date', dateInputFormat:'yyyy-MM-dd\'T\'HH:mm:ss.SSSX', dateOutputFormat: 'LLL do yyyy, HH:mm:ss.SSS' },
+        { label: 'Time', field: '@timestamp', tdClass: 'text-body2', type: 'date', dateInputFormat: "yyyy-MM-dd'T'HH:mm:ss.SSSX", dateOutputFormat: 'LLL do yyyy, HH:mm:ss.SSS' },
         { label: 'Method', field: 'method', tdClass: 'text-weight-bold' },
-        { label: 'Path', field: 'path', tdClass: 'text-weight-bold' },
-        { label: 'Requests', field: 'requests', type: 'number', tdClass: 'text-weight-bold' },
-        { label: 'Responses', field: 'responses', type: 'number' },
-        { label: 'Apdex Score', field: 'apdex_score', type: 'number', formatFn: this.formatToFixed2, tdClass: this.tdClassApdex },
-        { label: 'Errors', field: 'errors', type: 'number', tdClass: this.tdClassErrors },
-        { label: 'Req rate', field: 'req_rate', type: 'number', formatFn: this.formatToFixed2 },
-        { label: 'Err rate', field: 'err_rate', type: 'number', formatFn: this.formatToFixed2, tdClass: this.tdClassErrRate },
-        { label: 'Success', field: 'success', type: 'number' },
-        { label: 'Redirect', field: 'redirect', type: 'number' },
-        { label: 'Client Error', field: 'client_error', type: 'number', tdClass: this.tdClassCErr },
-        { label: 'Server Error', field: 'server_error', type: 'number', tdClass: this.tdClassSErr },
-        { label: 'Max Time (ms)', field: 'max_time', type: 'number' },
-        { label: 'Avg Time (ms)', field: 'avg_time', type: 'number', formatFn: this.formatToFixed2 },
-        { label: 'Avg Req Size', field: 'avg_req_clength', type: 'number', formatFn: this.formatToFixed0 },
-        { label: 'Avg Res Size', field: 'avg_res_clength', type: 'number', formatFn: this.formatToFixed0 },
-        { label: 'Tags', field: 'tags', type: 'string' }
+        { label: 'Path', field: 'api.path', tdClass: 'text-weight-bold' },
+        { label: 'URL', field: 'http.request.url' },
+        { label: 'Response Time', field: 'responsetime', type: 'number', tdClass: 'text-weight-bold' },
+        { label: 'Response Code', field: 'http.response.code', type: 'number', tdClass: 'text-weight-bold' }
       ],
       rows: [],
+      requestResponseRecord: '',
       ready: false
     };
   },
@@ -84,6 +87,7 @@ export default {
     }
   },
   mounted() {
+    Prism.manual = true;
     this.initialize();
     this.getStats({ fields: ['lasterrors'] });
     this.ready = true;
@@ -92,7 +96,9 @@ export default {
     ...mapActions({
       getStats: 'stats/getStats' // map `this.getStats()` to `... dispatch('getStats')`
     }),
-    initialize: function() {
+    initialize: function() {},
+    preRender: function(code) {
+      return code.replace(/\s+data-v-\S+="[^"]*"/g, '');
     },
     // TODO Reconsider
     loadStats: function() {
@@ -105,7 +111,12 @@ export default {
       //let requestsTotal = pathOr(0, ['all', 'requests'], statsContainer);
       this.rows = pathOr([], ['lasterrors'], statsContainer);
 
+      this.requestResponseRecord = JSON.stringify(this.rows[0], null, '\t');
+
       //this.loadStats();
+      this.$refs.code.textContent = this.preRender(this.requestResponseRecord, this);
+      Prism.highlightElement(this.$refs.code);
+      //Prism.highlightAll();
     }
   }
 };
