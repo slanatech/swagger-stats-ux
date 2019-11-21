a82145a73ad35170a79bc41e8bb29cd71d9fae1a
sv2 <sv2@slana.tech>
2019-11-20 22:06:16 -0800

    Initial

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
 </template>
 <style lang="scss">
 /* TODO Consider
+
+
 @import url('https://fonts.googleapis.com/css?family=Open+Sans:200,400,600,700');
 body {
   font-family: 'Open Sans', sans-serif;
